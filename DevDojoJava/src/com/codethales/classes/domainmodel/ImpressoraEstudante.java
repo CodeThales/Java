209ffc1084d7c9367e358a7fe0f992499cb6ee66
CodeThales <thalles.agency@gmail.com>
2022-02-01 21:28:17 -0300

    <feat>[classes.domainmodel]:<create ImpressoraEstudanteClass> [create imprime method]

---
+++ DevDojoJava/src/com/codethales/classes/domainmodel/ImpressoraEstudante.java
@@ -0,0 +1,11 @@
+package com.codethales.classes.domainmodel;
+
+public class ImpressoraEstudante {
+    public void imprime(Estudante estudante){
+
+        System.out.println("Name: " + estudante.getName());
+        System.out.println("Age: " + estudante.getAge());
+        System.out.println("Gender: " + estudante.getGender());
+
+    }
+}
