dcfc3e07ec39aa3cfe89e5cad536bc0b824fadb5
CodeThales <thalles.agency@gmail.com>
2022-02-04 20:36:27 -0300

    <feat>[sobrescrita]:<create classes and tests> [Create classes in order to understand how override works]

---
+++ DevDojoJava/src/com/codethales/sobrescrita/test/AnimeTest.java
@@ -0,0 +1,10 @@
+package com.codethales.sobrescrita.test;
+
+import com.codethales.sobrescrita.domainmodel.Anime;
+
+public class AnimeTest {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Dr. Stone");
+        System.out.println(anime);
+    }
+}
