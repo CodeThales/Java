52fc11096588637054230f595413a5b6b69df7e1
CodeThales <thalles.agency@gmail.com>
2022-02-07 10:37:47 -0300

    <feat>[classesabstratatas]:<create Desenvolvedor Class> Create  Desenvolvedor class in order to understand how abstract methods work.

---
+++ DevDojoJava/src/com/codethales/classesabstratas/domainmodel/Desenvolvedor.java
@@ -0,0 +1,21 @@
+package com.codethales.classesabstratas.domainmodel;
+
+public class Desenvolvedor extends Funcionario {
+
+    public Desenvolvedor(String name, double salary) {
+        super(name, salary);
+    }
+
+    @Override
+    public void calculaBonus() {
+        this.salary = this.salary + this.salary * 0.1;
+    }
+
+    @Override
+    public String toString() {
+        return "Desenvolvedor{" +
+                "name='" + name + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
